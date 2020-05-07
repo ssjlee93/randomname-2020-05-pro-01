db35887957a4dec422b7addb0cd8b5b712407d51
Steve S. Lee <sjl558@nyu.edu>
2020-05-07 18:44:06 -0400

    seven commit

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 var item = "";
-var city = "LA"; 
-var product = "eggs";
+var city = "";
+var product = "";
+var counter = 0;
+var responseObj = {};
+
+$(".city").on("click", function() {
+  city = $(this).attr("data-city");
+})
+$(".item").on("click", function() {
+  product = $(this).attr("data-item");
+})
+
+function search(event){
+  event.preventDefault();
+  prices();
+  nutrition();
+}
 
 function prices() {
-
 var settings = {
   "url": "https://grocerybear.com/getitems?city=" + city + "&product=" + product + "&num_days=7",
   "method": "POST",
   "timeout": 0,
   "headers": {
     "api-key": "D88BB3392503C0DB49C15ADA11395CD1BFD7886CF8CB5763FFF5A228A0EF93A0"
-
-  },
+  }
 };
 
-$.ajax(settings).done(function (response1) {
-  console.log(response1);
-  item = response1[0].title;
-  console.log(item)
-  
+$.ajax(settings).done(function (response) {
+  console.log(response);
+  console.log(city);
+  responseObj.price = response[counter].price; 
+  console.log(responseObj);
+
+ 
 });
 
 }
@@ -27,7 +42,7 @@ function nutrition() {
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://trackapi.nutritionix.com/v2/search/instant?query=" + item + "?field=nf_calories",
+    "url": "https://trackapi.nutritionix.com/v2/search/instant?query=" + product,
     "method": "GET",
     "headers": {
         "x-app-id": "84d7c19a",
@@ -36,9 +51,12 @@ function nutrition() {
 }
 $.ajax(settings).done(function (response) {
     console.log(response);
+    var thumbnail = response.common[counter].photo.thumb;
+   
+    $("#nutritionPhoto").attr("src",thumbnail);
+    
 });
 
 }
 
-prices();
-nutrition();
+$("#searchBtn").on("click",search);
